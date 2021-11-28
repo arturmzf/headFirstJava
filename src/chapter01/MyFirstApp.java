7d469232e053f097322abd6ea0358d5c22c8142f
arturmzf <artur.mzf@gmail.com>
2021-11-28 19:41:18 +0300

    DooBee Program

---
+++ src/chapter01/MyFirstApp.java
@@ -0,0 +1,8 @@
+package chapter01;
+
+public class MyFirstApp{
+	public static void main(String[] args){
+        System.out.println("I control");
+        System.out.println("the World!");
+	}
+}
---
+++ src/chapter01/Party.java
@@ -0,0 +1,20 @@
+package chapter01;
+
+import java.awt.*;
+
+import java.awt.event.*;
+
+class Party{
+	public void buildInvite(){
+
+		Frame f = new Frame();
+		Label l = new Label("Вечеринка у Тома");
+		Button b = new Button("Ваша ставка");
+		Button c = new Button("Сбросить");
+		Panel p = new Panel();
+
+		p.add(l);
+	}
+
+	// ещё код...
+}
